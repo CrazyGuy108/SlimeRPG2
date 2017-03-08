8f4493f3e5e10f9947f93cc401b650123b217deb
CrazyGuy108 <taylor@hansenfamily.us>
2017-03-07 22:25:38 -0800

    Use size_t for location index instead of uint16_t
    Usually, std::vector expects parameters of type size_t. I'm not going to go
    into the case where it's better to use size_type for now, but probably later.

--- src/player.cpp
+++ src/player.cpp
@@ -3,7 +3,7 @@
 Player::Player(std::string name, uint16_t currentHealth,
 	uint16_t maxHealth, uint16_t level, uint16_t experience,
 	uint16_t attack, uint16_t defense, uint16_t expRequired,
-	uint16_t location)
+	size_t location)
 	: Entity{ name, currentHealth, maxHealth, level, experience, attack,
 		defense },
 	expRequired{ expRequired },
@@ -20,7 +20,7 @@ uint16_t Player::getExpRequired() const noexcept
 	return expRequired;
 }
 
-uint16_t Player::getLocation() const noexcept
+size_t Player::getLocation() const noexcept
 {
 	return location;
 }
--- src/player.hpp
+++ src/player.hpp
@@ -11,11 +11,11 @@ public:
 	Player(std::string name, uint16_t currentHealth,
 		uint16_t maxHealth, uint16_t level, uint16_t experience,
 		uint16_t attack, uint16_t defense, uint16_t expRequired,
-		uint16_t location);
+		size_t location);
 	virtual ~Player() override;
 
 	uint16_t getExpRequired() const noexcept;
-	uint16_t getLocation() const noexcept;
+	size_t getLocation() const noexcept;
 
 	// gain an amount of experience, returning the amount of levels gained
 	bool gainExperience(uint16_t experience);
@@ -31,7 +31,7 @@ private:
 	// experience required for the next level
 	uint16_t expRequired;
 	// location id
-	uint16_t location;
+	size_t location;
 };
 
 #endif // PLAYER_HPP
